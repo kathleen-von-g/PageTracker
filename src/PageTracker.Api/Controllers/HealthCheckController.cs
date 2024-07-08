b914c918fde812ce9f692beb0fbc2c3dc6afa937
Kathleen von Gnielinski <kathleen.vongnielinski@flightcentre.com>
2024-07-09 07:53:46 +1000

    Editor config for file scoped namespaces

---
+++ src/PageTracker.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,11 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PageTracker.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[ApiVersion("1.0")]
+public class HealthCheckController : ControllerBase
+{
+}
