2303dd0061c74242ce1414d9ab185261671bc4bd
Kathleen von Gnielinski <kathleen.vongnielinski@outlook.com>
2024-07-24 10:29:40 +1000

    Add "Record Page Finished At" endpoint

--- src/PageTracker.Application/ReadingSessions/ReadingSessionService.cs
+++ src/PageTracker.Application/ReadingSessions/ReadingSessionService.cs
@@ -20,11 +20,19 @@ namespace PageTracker.Application.ReadingSessions
         Task<int> GetNumberOfPagesRead(DateTimeOffset dateToRetrieve, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Creates a reading session where you have read the provided number of pages on the current day
+        /// Saves the page number that the reader was on when they finished their reading session and then calculates<br/>
+        /// the number of pages read since the last recorded session.
         /// </summary>
-        /// <param name="numberOfPages">The number of full pages you've read. Must be 0 or more.</param>
+        /// <param name="pageNumber">The page number the reader was on when they finished their reading session</param>
+        Task<ReadingSession> RecordPageFinishedAt(int pageNumber, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Creates a reading session where the reader has read the provided number of pages on the current day
+        /// </summary>
+        /// <param name="numberOfPages">The number of full pages the reader has read. Must be 0 or more.</param>
         Task<ReadingSession> RecordPages(int numberOfPages, CancellationToken cancellationToken = default);
     }
+
     internal class ReadingSessionService(ILogger<ReadingSessionService> logger, IPageTrackerDbContext context, TimeProvider timeProvider) : IReadingSessionService
     {
         private const int MininumNumberOfPages = 0;
@@ -42,6 +50,11 @@ namespace PageTracker.Application.ReadingSessions
             logger.LogInformation(message);
 
             return readingSessions.Sum(x => x.NumberOfPages);
+        }
+
+        public Task<ReadingSession> RecordPageFinishedAt(int pageNumber, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
         }
 
         public async Task<ReadingSession> RecordPages(int numberOfPages, CancellationToken cancellationToken = default)
